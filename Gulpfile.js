58f0e9babf79b622fe0c0b5d179f7bc6ab5d5715
Oleg verhovsky <verchol@gmail.com>
2016-08-01 09:34:01 +0300

    add bump

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,13 @@
 var spawn = require('child_process').spawn;
 var gulp  = require('gulp');
+var bump = require('gulp-bump');
 
 gulp.task('npm', function (done) {
   spawn('npm', ['publish'], { stdio: 'inherit' }).on('close', done);
+});
+
+gulp.task('bump', function(){
+  gulp.src('./package.json')
+  .pipe(bump({type:'minor'}))
+  .pipe(gulp.dest('./'));
 });
