687c1646d0fbeb0ef95e870efb0f20d3b55efcab
nikolai <n.ivanov.p@gmail.com>
2017-02-15 21:40:19 +0300

    trigger default pipeline with default branch

--- commands/builds/command.js
+++ commands/builds/command.js
@@ -4,6 +4,7 @@ var Q           = require('q');
 var request     = require('request');
 var prettyjson  = require('prettyjson');
 var pipelines   = require('../pipelines/command');
+var repos       = require('../repos/command');
 var Build       = require('./build');
 var helper      = require('../../helper/helper');
 
@@ -45,7 +46,6 @@ var getBuildById = function (info) {
         if (err) {
             deferred.reject(err);
         }
-        // console.log('body:' + JSON.stringify(body));
         deferred.resolve(new Build.Build(JSON.parse(body)));
     });
     return deferred.promise;
@@ -58,7 +58,8 @@ var followBuildProgress = function (data) {
     var repeat = () => {
         getBuildById(data)
             .then((build) => {
-                if(build.getStatus() === 'start' || build.getStatus() === 'running') {
+                var status = build.getStatus();
+                if(status === 'start' || status === 'pending' || status === 'elected') {
                     setTimeout(() => repeat(), 1000);
                 } else {
                     process.stdout.write(' done!\n');
@@ -66,6 +67,7 @@ var followBuildProgress = function (data) {
                     deferred.resolve(build);
                 }
             }, (err) => {
+                console.log('error:' + err);
                 deferred.reject(err);
             });
     };
@@ -86,6 +88,46 @@ module.exports.build = function (info) {
 
                     followBuildProgress(info).then(function (res) {
                         console.log('Build Status: ' + res.getStatus());
+                    }, (err) => {
+                        throw new Error(err);
+                    });
+                }, (err) => {
+                    throw new Error(err);
+                });
+            }, (err) => {
+                throw new Error(err);
+            });
+    };
+};
+
+module.exports.buildDefaultPipeline = function (info) {
+    return (token) => {
+        info.token = token;
+        // get default pipeline
+        pipelines.getDefaultPipeline(info)
+            .then(function (res) {
+                info.pipelineName = res.getName();
+                pipelines.getPipelineByName(info).then((res_pipeline_id) => {
+                    info.pipelineId = res_pipeline_id.getId();
+                    // get default branch of repo
+                    repos.getRepo(info).then((res_repo) => {
+                        if(!info.branch) {
+                            info.branch = res_repo.getDefaultBranch();
+                        }
+                        // build pipeline
+                        buildByService(info).then(function (res_service) {
+                            info.buildId = res_service;
+
+                            console.log(`Build url: ${info.url}/repositories/${info.repoOwner}/${encodeURIComponent(info.repoName)}/builds/${info.buildId}`);
+
+                            followBuildProgress(info).then(function (res_progress) {
+                                console.log('Build Status: ' + res_progress.getStatus());
+                            }, (err) => {
+                                throw new Error(err);
+                            });
+                        }, (err) => {
+                            throw new Error(err);
+                        });
                     }, (err) => {
                         throw new Error(err);
                     });
