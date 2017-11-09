f02a1fab8e1b7bbfd8e4ff12199a04d6401ee7ba
Codefresh <ziv@codefresh.io>
2017-11-09 10:45:06 +0200

    add tests

--- commands/pipelines/cmd/get.spec.js
+++ commands/pipelines/cmd/get.spec.js
@@ -10,25 +10,40 @@ chai.use(require('chai-as-promised'));
 
 describe('run pipline test', () => {
 
-    it('run pipeline success', (done) => {
+    it('run pipeline success', () => {
         var info = {
+            url: 'https://g.codefresh.io',
+            branch : 'master',
+            noCache : false,
+            resetVolume : false,
+            variables : [],
             repoOwner:'codefresh-io',
             repoName: 'demochat',
             pipelineName: 'demochat',
             token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1OWQzNzgzNzkyMGY4OTAwMDE4YTYxODIiLCJhY2NvdW50SWQiOiI1OWQzNzgzNzkyMGY4OTAwMDE4YTYxODMiLCJpYXQiOjE1MDkzNTAwMDcsImV4cCI6MTUxMTk0MjAwN30.4DwyRb-71XZDXvnFJAe3cQnQp34FK0Q2jnne9eVpYJk'
         };
-        return expect(commands.executePipeline(info)).to.eventually.be.rejected;
-    });
+        return commands.executePipeline(info).then(function(res){
+            expect(res).to.not.equal({});
+        });
+    }).timeout(3000);
 
-    it('run pipeline fails', (done) => {
+
+    it('run pipeline fails', () => {
         var info = {
+            url: 'https://g.codefresh.io',
+            branch : 'master',
+            noCache : false,
+            resetVolume : false,
+            variables : [],
             repoOwner:'codefresh-io',
-            repoName: 'ziv',
             pipelineName: 'demochat',
             token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1OWQzNzgzNzkyMGY4OTAwMDE4YTYxODIiLCJhY2NvdW50SWQiOiI1OWQzNzgzNzkyMGY4OTAwMDE4YTYxODMiLCJpYXQiOjE1MDkzNTAwMDcsImV4cCI6MTUxMTk0MjAwN30.4DwyRb-71XZDXvnFJAe3cQnQp34FK0Q2jnne9eVpYJk'
         };
-        return expect(commands.executePipeline(info)).to.eventually.be.rejected;
-    });
+        return commands.executePipeline(info).then(function(res){
+        },(err)=>{
+            expect(err.message).to.be.equal("you must provide [pipelineid] or [pipelineName] [repoName] [repoOwner]");
+        });
+    }).timeout(3000);
 
 
 
