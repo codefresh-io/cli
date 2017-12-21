413447bd2ea36dcc99fc314dd9094c10b67bc30d
Itai Gendler <itaigendler@Itais-MacBook-Pro.local>
2017-12-22 01:25:44 +0200

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        "indent": ["error", 4],
+    'plugins': [
+        'mocha'
+    ],
+    'extends': 'airbnb-base',
+    'rules': {
+        'indent': ['error', 4, { 'SwitchCase': 1 }],
+        'no-underscore-dangle': [0],
+        'max-len': ['error', {
+            'code': 140,
+            'ignoreComments': true
+        }],
+    },
+    'env': {
+        'mocha': true,
     }
 };
