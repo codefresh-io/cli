e036860aac51d9d125fce7f727efe2f3fda0dd4a
Kostis (Codefresh) <39800303+kostis-codefresh@users.noreply.github.com>
2018-11-28 15:55:24 +0200

    Fixed download links after new alpine addition (#235)

--- docs/content/installation/_index.md
+++ docs/content/installation/_index.md
@@ -8,6 +8,6 @@ Codefresh CLI is developed and built with node.js. <br />
 
 In case you have node.js installed on your machine you can use [NPM](/cli/installation/npm) or [YARN](/cli/installation/yarn) to install it.<br>
 
-If you don't have node.js installed you can install the CLI by [Donwloading](/cli/installation/download) it directly.<br />
+If you don't have node.js installed you can install the CLI by [Downloading](/cli/installation/download) it directly.<br />
 
 It is also possible to run the CLI using a [Docker image](/cli/installation/docker).
--- docs/index.js
+++ docs/index.js
@@ -365,9 +365,10 @@ const createDownloadPage = async () => {
             'and download the binary that matches your operating system.<br>\n' +
             'We currently support the following OS: <br>\n' +
             '<ul>\n' +
-            '    <li><a href=' + links[0] + ' target="_blank">Linux-x64</a></li>\n' +
-            '    <li><a href=' + links[1] + ' target="_blank">Macos-x64</a></li>\n' +
-            '    <li><a href=' + links[2] + ' target="_blank">Windows-x64</a></li>\n' +
+            '    <li><a href=' + links[0] + ' target="_blank">Alpine-x64</a></li>\n' +
+            '    <li><a href=' + links[1] + ' target="_blank">Linux-x64</a></li>\n' +
+            '    <li><a href=' + links[2] + ' target="_blank">Macos-x64</a></li>\n' +
+            '    <li><a href=' + links[3] + ' target="_blank">Windows-x64</a></li>\n' +
             '</ul> \n' +
             '\n' +
             'After downloading the binary, untar or unzip it and your are good to go.<br>\n' +
