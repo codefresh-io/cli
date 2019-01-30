7c8872f5471e1fb7f295ac87664d982e7d52a51d
yaroslav-codefresh <45195052+yaroslav-codefresh@users.noreply.github.com>
2019-01-30 14:22:28 +0200

    saas-1568 - node version check (#267)
    * version check
    
    * proper package.json for check

---
+++ check-version.js
@@ -0,0 +1,8 @@
+const semver = require('semver');
+const { engines } = require('./package');
+
+const version = engines.node;
+if (!semver.satisfies(process.version, version)) {
+    throw new Error(`Required node version ${version} not satisfied with current version ${process.version}.`);
+}
+
