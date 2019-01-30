40e69c72310dce5793faf94e946f57d1e8095307
yaroslav-codefresh <yarosav@codefresh.io>
2019-01-30 12:30:31 +0200

    test check

--- check-version.js
+++ check-version.js
@@ -3,7 +3,6 @@ const { engines } = require('./package');
 
 const version = engines.node;
 if (!semver.satisfies(process.version, version)) {
-    console.log(`Required node version ${version} not satisfied with current version ${process.version}.`);
-    process.exit(1);
+    throw new Error(`Required node version ${version} not satisfied with current version ${process.version}.`);
 }
 
