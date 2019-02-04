3988db71724786d055ebd2049980abec70d90499
yaroslav-codefresh <yarosav@codefresh.io>
2019-02-04 11:58:27 +0200

    remove run command

--- check-version.js
+++ check-version.js
@@ -1,9 +1,8 @@
-// const semver = require('semver');
-// const { engines } = require('./package');
-//
-// const version = engines.node;
-// if (!semver.satisfies(process.version, version)) {
-//     throw new Error(`Required node version ${version} not satisfied with current version ${process.version}.`);
-// }
-console.log("aaa");
+const semver = require('semver');
+const { engines } = require('./package');
+
+const version = engines.node;
+if (!semver.satisfies(process.version, version)) {
+    throw new Error(`Required node version ${version} not satisfied with current version ${process.version}.`);
+}
 
