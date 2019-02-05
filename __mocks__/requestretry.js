c3e8a131f98d48eeb64cc7d08551b492390a7f4f
yaroslav-codefresh <yarosav@codefresh.io>
2019-02-05 16:59:28 +0200

    refactor tests

--- __mocks__/requestretry.js
+++ __mocks__/requestretry.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 
-const DEFAULT_RESPONSE = { statusCode: 200 };
+const DEFAULT_RESPONSE = { statusCode: 200, body: { test: 'test' } };
 
 let RESPONSE = _.clone(DEFAULT_RESPONSE);
 
@@ -17,6 +17,10 @@ request.__setResponse = (response) => {
 
 request.__resetResponse = () => {
     RESPONSE = _.clone(DEFAULT_RESPONSE);
+};
+
+request.__defaultResponse = () => {
+    return _.clone(DEFAULT_RESPONSE);
 };
 
 module.exports = request;
--- test-setup.js
+++ test-setup.js
@@ -1,17 +1,11 @@
 const sdk = require('./lib/logic/sdk');
 const openapi = require('./openapi');
+const Output = require('./lib/output/Output');
+const _ = require('lodash');
 
-jest.mock('./lib/output/Output', () => {
-    return {
-        print: () => {
-        },
-        printError: (e) => {
-            throw e;
-        },
-        init: () => {
-        },
-    };
-});
+jest.mock('./lib/output/Output');
+
+jest.mock('./lib/logic/entities/Board');
 
 sdk.configure({
     url: 'http://not.needed',
@@ -19,4 +13,24 @@ sdk.configure({
     apiKey: 'not-needed',
 });
 
-module.exports = {};
+class NotThrownError extends Error {
+}
+
+global.expectOutputEquals = (data) => {
+    expect(_.first(_.last(Output.print.mock.calls))).toEqual(data);
+};
+
+global.expectThrows = async (func, ExpectedError) => {
+    try {
+        await func();
+        throw new NotThrownError();
+    } catch (e) {
+        if (e instanceof NotThrownError) {
+            throw e;
+        }
+        if (ExpectedError && !(e instanceof ExpectedError)) {
+            throw e;
+        }
+        return e;
+    }
+};
