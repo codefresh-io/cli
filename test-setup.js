99a3995f511a03fc73da42269cc351f67bf7007a
yaroslav-codefresh <yarosav@codefresh.io>
2019-02-27 10:55:24 +0200

    return auth changes

--- test-setup.js
+++ test-setup.js
@@ -1,15 +1,13 @@
 const _ = require('lodash');
 const request = require('requestretry');
 const sdk = require('./lib/logic/sdk');
+const { Config } = require('codefresh-sdk');
 const openapi = require('./openapi');
 
-
-jest.mock('./lib/output/Output'); // eslint
+jest.mock('./lib/output/Output');
+jest.mock('codefresh-sdk/lib/auth/contexts/whoami');
 
 let SDK_CONFIGURED;
-
-class NotThrownError extends Error {
-}
 
 global.verifyResponsesReturned = async (responses) => {
     const { results } = request.mock;
@@ -18,31 +16,16 @@ global.verifyResponsesReturned = async (responses) => {
     expect(returnedResponses).toEqual(responses);
 };
 
-global.expectThrows = async (func, ExpectedError) => {
-    try {
-        await func();
-        throw new NotThrownError('Expected error not thrown!');
-    } catch (e) {
-        if (e instanceof NotThrownError) {
-            throw e;
-        }
-        if (ExpectedError && !(e instanceof ExpectedError)) {
-            throw e;
-        }
-        return e;
-    }
-};
-
 /**
  * downloads spec one time for all tests
  * */
 global.configureSdk = async () => {
     if (!SDK_CONFIGURED) {
         SDK_CONFIGURED = true;
-        sdk.configure({
+        sdk.configure(await Config.load({
             url: 'http://not.needed',
             apiKey: 'not-needed',
-            spec: openapi,
-        });
+            spec: { json: openapi },
+        }));
     }
 };
